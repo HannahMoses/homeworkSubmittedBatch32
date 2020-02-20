f99a6be172d9eea944109733369b13c5d9852b3a
HannahMungamuri <25067039+HannahMoses@users.noreply.github.com>
2020-02-19 22:30:55 -0600

    Update TestDuration.java

--- HW19feb2020/src/TestDuration.java
+++ HW19feb2020/src/TestDuration.java
@@ -6,7 +6,7 @@ public class TestDuration {
 //homework to write a Java program to calculate duration taken to run this program estimate efficiency
 /*
 
-
+Experimenting with git pull command 
  
   
 Fill this comment space  */
